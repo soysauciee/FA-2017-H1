a69835d66525d2473af401555f2e8d5e41f52ef8
student <student@ssu-cs-dar28-167-205.sonoma.edu>
2017-08-31 20:05:17 -0700

    first commit

---
+++ Gerard385FAH1/app/src/main/java/com/fa17/ssu385/gerard385fah1/StubAcivity.java
@@ -0,0 +1,13 @@
+package com.fa17.ssu385.gerard385fah1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StubAcivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stub_acivity);
+    }
+}
