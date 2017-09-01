a717f53284afc4a9fde46b0d440172d5f8c4b3a3
student <student@ssu-cs-dar25-167-138.sonoma.edu>
2017-08-31 19:59:32 -0700

    first commit

---
+++ Tran385FAH1/app/src/main/java/com/fa17/ssu385/tran385fah1/StubActivity.java
@@ -0,0 +1,13 @@
+package com.fa17.ssu385.tran385fah1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stub);
+    }
+}
