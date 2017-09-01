f0d01fa0c0f75e1762a69df23bb85f6240b578a3
student <student@ssu-cs-dar25-167-150.sonoma.edu>
2017-08-31 19:53:06 -0700

    first commit

---
+++ Marshall385FAH1/app/src/main/java/com/fa17/ssu385/marshall385fah1/StubAvtivity.java
@@ -0,0 +1,13 @@
+package com.fa17.ssu385.marshall385fah1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StubAvtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stub_avtivity);
+    }
+}
